553a16e3531b79ed276f2e8233702d02b9e0f81a
vzz <vzz@uw.edu>
2021-12-15 22:44:37 -0800

    fail to get gcloud deploy

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ data = {
     "color_data_input": "",
     "color_data_output": "",
     "theme_color_series": [],
-    "error": ""
+    "error": "",
+    "server_error": ""
 }
 
 @app.route("/")
 def homepage():
     data["error"] = ""
+    data["server_error"] = ""
     return render_template('index.html', data=data)
 
 @app.route("/random")
 def random_handler():
     data["error"] = ""
+    data["server_error"] = ""
     data["random_color"] = []
     random_count = request.args.get("rand_num")
     color_str = safe_get(RANDOM_API + "?number=" + str(random_count))
@@ -35,6 +38,7 @@ def random_handler():
 @app.route("/transfer")
 def transfer_handler():
     data["error"] = ""
+    data["server_error"] = ""
     input_type = request.args.get("input_type")
     output_type = request.args.get("output_type")
     data["color_data_input"] = request.args.get("input_data")
@@ -44,46 +48,53 @@ def transfer_handler():
     else:
         request_url = TRANSFER_API_BASE + input_type + "2" + output_type + "?value=" + data["color_data_input"]
         output_str = safe_get(request_url)
-        output_data = json.loads(output_str)
-        if "error" in output_data.keys():
-            data["color_data_output"] = "Please enter the correct format of color."
+        if output_str is None:
+            data["error"] = "Invalid Input on color for transferring."
+
+
         else:
+            output_data = json.loads(output_str)
             data["color_data_output"] = output_data[output_type]
     return render_template('index.html', data=data)
 
 @app.route("/series")
 def series_handler():
     data["error"] = ""
+    data["server_error"] = ""
     input_red = request.args.get("input_red")
     input_green = request.args.get("input_green")
     input_blue = request.args.get("input_blue")
 
     if input_red == '' or input_green == '' or input_blue == '':
+        data["error"] = "Don't enter empty value while getting a series of color!"
+        return render_template('index.html', data=data)
+
+    try:
+        input_red = int(input_red)
+        input_green = int(input_green)
+        input_blue = int(input_blue)
+
+        second_color_str = safe_get(RANDOM_API)
+        if second_color_str is not None:
+            second_color_data = json.loads(second_color_str)["rgb"][4:-1]
+            color1 = [input_red, input_green, input_blue]
+            color2 = second_color_data.split(", ")
+            color2 = [int(num) for num in color2]
+            data["theme_color_series"] = gen_theme_colors(color1, color2)
+        return render_template('index.html', data=data)
+    except ValueError:
         data["error"] = "Invalid Input"
         return render_template('index.html', data=data)
 
-    input_red = int(input_red)
-    input_green = int(input_green)
-    input_blue = int(input_blue)
-    second_color_str = safe_get(RANDOM_API)
-    if second_color_str is not None:
-        second_color_data = json.loads(second_color_str)["rgb"][4:-1]
-        color1 = [input_red, input_green, input_blue]
-        color2 = second_color_data.split(", ")
-        color2 = [int(num) for num in color2]
-        data["theme_color_series"] = gen_theme_colors(color1, color2)
-    return render_template('index.html', data=data)
 
 def safe_get(url):
     try:
         return urllib.request.urlopen(url).read().decode('utf-8')
     except urllib.error.URLError as e:
         if hasattr(e, "code"):
-            print("The server couldn't fulfill the request.")
-            print("Error code: ", e.code)
+            data["server_error"] = "The server couldn't fulfill the request. Error code: " + str(e.code)
         elif hasattr(e, 'reason'):
-            print("We failed to reach a server")
-            print("Reason: ", e.reason)
+            data["server_error"] = "We failed to reach a server. Reason: " + str(e.reason)
         return None
 
 # two points; find a linear function that goes through them with in (255, 255, 255).
@@ -107,7 +118,6 @@ def gen_theme_colors(color1, color2, number=5):
     for i in range(points_between_num):
         ratios.append(smaller_ratio + interval * (i + 1))
 
-    print(ratios)
     while len(ratios) < number:
         if smaller_ratio - interval >= 0:
             smaller_ratio -= interval
@@ -126,10 +136,12 @@ def gen_theme_colors(color1, color2, number=5):
         current_step = [round(ratio_diff * step_vector[i]) for i in range(len(step_vector))]
         cur_color = []
         for i in range(3):
-            cur_color.append(str(color1[i] + current_step[i]))
+            rgb_sum = color1[i] + current_step[i]
+            rgb_sum = rgb_sum if rgb_sum >= 0 else 0
+            rgb_sum = rgb_sum if rgb_sum <= 255 else 255
+            cur_color.append(str(rgb_sum))
         colors.append("rgb(" + ', '.join(cur_color) + ")")
     return colors
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
-
